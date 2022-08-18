1eb9fe8c8cf3562b79525facfc665922ac306581
slawagn <slawagn@gmail.com>
2022-08-18 19:34:35 +0300

    init

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,45 @@
+class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[update show destroy edit]
+  def create 
+    question = Question.create(question_params)
+
+    redirect_to question_path(question), notice: 'Новый вопрос создан!'
+  end
+
+  def update
+    @question.update(question_params)
+
+    redirect_to question_path(@question), notice: 'Сохранили вопрос!'
+  end
+
+  def destroy
+    @question.destroy
+
+    redirect_to questions_path, notice: 'Вопрос удалён!'
+  end
+
+  def show
+  end
+
+  def index
+    @question = Question.new
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body, :user_id)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+end
