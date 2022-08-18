97995457afab025667060df31dfecbdc2e415b06
slawagn <slawagn@gmail.com>
2022-08-18 19:37:33 +0300

    auto login on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,16 @@ class UsersController < ApplicationController
     session[:current_time] = Time.now
     @user = User.new
   end
-
+  
   def create
     user_params = params.require(:user).permit(:name, :nickname, :email, :password)
 
-    User.create(user_params)
+    if (user = User.create(user_params))
+      session[:user_id] = user.id
 
-    redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+      redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    else
+      redirect_to new_user_path, notice: 'Что-то пошло не так!' 
+    end
   end
 end
